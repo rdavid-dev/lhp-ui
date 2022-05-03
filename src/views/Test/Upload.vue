24e3badefddca3c9c7be728ff1d5bcfbc6b1167b
Reence <david.reence@gmail.com>
2022-05-04 02:01:38 +0800

    UPdate Set

--- src/views/Test/Upload.vue
+++ src/views/Test/Upload.vue
@@ -1,15 +1,33 @@
 <template>
-    <UploadFileDocument />
+  <UploadFileDocument ref="upload" />
+  <button @click="uploadFiles">Upload Document</button>
 </template>
 <script>
-import UploadFileDocument from "@/components/UploadFileDocument"
+import axios from "axios";
+import UploadFileDocument from "@/components/UploadFileDocument";
 
 export default {
-    components: {
-        UploadFileDocument
+  components: {
+    UploadFileDocument,
+  },
+  setup() {},
+  methods: {
+    uploadFiles() {
+      const filesSelected = this.$refs.upload.selectedFiles;
+      const formData = new FormData();
+      for (const i of Object.keys(filesSelected)) {
+        formData.append(`attachments[${i}]`, filesSelected[i]);
+      }
+      axios
+        .post("http://localhost:5010/api/v1/file-upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        });
     },
-    setup () {
-
-    }
-}
+  },
+};
 </script>
